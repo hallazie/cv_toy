0143edc2396267a9542294bd686a306695812025
hallazie <hallazie@outlook.com>
2019-06-04 01:09:34 +0800

    --amend

---
+++ saliency/README.md
@@ -0,0 +1,7 @@
+## Saliency Model Implementation
+
+### LWSAL: Lightweighted saliency model
+
+[Light-weighted Saliency Detection with Distinctively Lower Memory Cost and Model Size](https://arxiv.org/abs/1901.05002)
+
+Reimplementation of the model by Pytorch.
---
+++ saliency/lwsal/py/model.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+INPLACE = True
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.coarse = self.SubNet()
+        self.fine = self.SubNet()
+
+    def SubNet(self):
+        return nn.Sequential(
+            self.Conv(3, 32, 3),
+            self.Pool('max'),
+            self.ConvDWS(32, 16, 64),
+            self.ConvDWS(16, 16, 64),
+            self.Pool('max'),
+            self.ConvDWS(16, 24, 96),
+            self.ConvDWS(24, 24, 96),
+            self.ConvDWS(24, 24, 96),
+            self.Pool('max'),
+            self.ConvDWS(24, 32, 128),
+            self.ConvDWS(32, 32, 128),
+            self.ConvDWS(32, 32, 128),
+            self.ConvDWS(32, 32, 128),
+            self.ConvDWS(32, 64, 256),
+            self.ConvDWS(64, 64, 256),
+            self.ConvDWS(64, 128, 512),
+            self.Conv(128, 1, 1)
+        )
+
+    def ConvDWS(self, c_in, c_out, exp):
+        return nn.Sequential(
+            nn.Conv2d(c_in, exp, 1, groups=1),
+            nn.BatchNorm2d(exp),
+            nn.ReLU(inplace=INPLACE),
+            nn.Conv2d(exp, exp, 3, groups=exp),
+            nn.BatchNorm2d(exp),
+            nn.ReLU(inplace=INPLACE),
+            nn.Conv2d(exp, c_out, 1, groups=1),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(inplace=INPLACE)
+        )
+
+    def Conv(self, c_in, c_out, ksize):
+        return nn.Sequential(
+            nn.Conv2d(c_in, c_out, kernel_size=ksize),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(inplace=INPLACE)
+        )
+
+    def Pool(self, typ):
+        if typ == 'max':
+            return nn.MaxPool2d(2, stride=2)
+
+def train():
+    net = Net().cuda()
+
+if __name__ == '__main__':
+    train()
