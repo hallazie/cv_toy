7c484e493f4e1abd6cfd09cc3563e53a94d09eb2
肖尚华 <hallazie@gmail.com>
2019-11-20 01:23:01 +0800

    --amend

--- prunning/saliency/resnetsal.py
+++ prunning/saliency/resnetsal.py
@@ -1,97 +1,129 @@
 '''
-    This is an implementation based on  the Light Weight Saleincy of Shanghua with some modifications
+ResNet Saliency a baseline model with ResNet 50 backbone
 
-    @author: Xiao Shanghua
-
-    File modified significantly to fit to the framework
+@author: Hamed R. Tavakoli
 '''
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-INPLACE = True
+from torchvision.models.resnet import resnet50, Bottleneck
 
+from thop import profile
 
-class InvertedResidual(nn.Module):
+class RusidualBlock(nn.Module):
 
-    def __init__(self, c_in, c_out, exp):
-        super(InvertedResidual, self).__init__()
-        self.use_res = c_in == c_out
-        self.conv = nn.Sequential(
-            nn.Conv2d(c_in, exp, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(exp),
-            nn.ReLU6(inplace=INPLACE),
-            nn.Conv2d(exp, exp, kernel_size=3, stride=1, padding=1, groups=exp, bias=False),
-            nn.BatchNorm2d(exp),
-            nn.ReLU6(inplace=INPLACE),
-            nn.Conv2d(exp, c_out, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.ReLU6(inplace=INPLACE)
-        )
+    def __init__(self, inp, out, exp, droprate):
+        super(RusidualBlock, self).__init__()
+        self.res_flag = inp == out
+        inp = int(inp * droprate)
+        out = int(out * droprate)
+        mid = int((out * droprate) // exp)
+        self.conv1 = nn.Conv2d(inp, exp, kernel_size=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(exp)
+        self.conv2 = nn.Conv2d(exp, exp, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(exp)
+        self.conv3 = nn.Conv2d(exp, out, kernel_size=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out)
+        self.relu = nn.ReLU(inplace=True)
+        self.convr = nn.Conv2d(inp, out, kernel_size=1, padding=0)
+        self.bnr = nn.BatchNorm2d(out)
 
     def forward(self, x):
-        if self.use_res:
-            return x + self.conv(x)
-        else:
-            return self.conv(x)
+        residual = x
+        if self.res_flag:
+            residual = self.convr(residual)
+            residual = self.bnr(residual)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x += residual
+        x = self.relu(x)
+        return x
 
+class _ScaleUp(nn.Module):
+
+    def __init__(self, in_size, out_size):
+        super(_ScaleUp, self).__init__()
+
+        self.up = nn.Sequential(
+            nn.ConvTranspose2d(in_size, in_size, kernel_size=2, stride=2),
+            nn.BatchNorm2d(in_size),
+            nn.LeakyReLU(inplace=True))
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_size, out_size, kernel_size=3, stride=1, padding=2, dilation=2),
+            nn.BatchNorm2d(out_size),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(out_size, out_size, kernel_size=1, stride=1),
+            nn.LeakyReLU(inplace=True),
+        )
+
+        self.__init_weights__()
+
+    def __init_weights__(self):
+
+        nn.init.kaiming_normal_(self.conv[0].weight)
+        nn.init.constant_(self.conv[0].bias, 0.0)
+        nn.init.kaiming_normal_(self.conv[3].weight)
+        nn.init.constant_(self.conv[3].bias, 0.0)
+
+        nn.init.kaiming_normal_(self.up[0].weight)
+        nn.init.constant_(self.up[0].bias, 0.0)
+
+    def forward(self, inputs):
+        output = self.up(inputs)
+        output = self.conv(output)
+        return output
+
+def flat_bottleneck(bottleneck):
+    for x in bottleneck.modules():
+        print(x)
+    print('----------')
 
 class Model(nn.Module):
 
-    def __init__(self):
+    def __init__(self, ):
         super(Model, self).__init__()
-        self.fine_net = self.SubNet()
+        self.encode_image = resnet50(pretrained=True)
+        modules = list(self.encode_image.children())[:-2]
+        modules_flat = []
 
-    def forward(self, fine_batch):
+        for m in self.encode_image.modules():
+            if type(m) == Bottleneck:
+                flat_bottleneck(m)
 
-        fine = self.fine_net(fine_batch)
-        # coarse = F.interpolate(self.coarse_net(F.interpolate(fine_batch, scale_factor=0.5)), scale_factor=2)
-        # mul = coarse * fine
-        return fine
+        self.encode_image = nn.Sequential(*modules)
+        self.decoder1 = _ScaleUp(2048, 1024)
+        self.decoder2 = _ScaleUp(1024, 512)
+        self.decoder3 = _ScaleUp(512, 256)
+        self.saliency = nn.Conv2d(256, 1, kernel_size=1, stride=1, padding=0)
 
-    def SubNet(self):
-        return nn.Sequential(
-            self.Conv(3, 32, 3, 1),
-            self.Conv(32, 32, 3, 1),
-            self.Pool('max'),
-            InvertedResidual(32, 16, 64),
-            InvertedResidual(16, 16, 64),
-            self.Pool('max'),
-            InvertedResidual(16, 24, 96),
-            InvertedResidual(24, 24, 96),
-            InvertedResidual(24, 24, 96),
-            self.Pool('max'),
-            InvertedResidual(24, 32, 128),
-            InvertedResidual(32, 32, 128),
-            InvertedResidual(32, 32, 128),
-            InvertedResidual(32, 32, 128),
-            InvertedResidual(32, 64, 256),
-            InvertedResidual(64, 64, 256),
-            InvertedResidual(64, 128, 512),
-            InvertedResidual(128, 128, 512),
-            nn.Conv2d(128, 1, kernel_size=1),
-            nn.ReLU6(inplace=INPLACE)
-        )
+        self.__init_weights__()
 
-    def Conv(self, c_in, c_out, ksize, pad):
-        return nn.Sequential(
-            nn.Conv2d(c_in, c_out, kernel_size=ksize, stride=1, padding=pad, bias=False),
-            nn.BatchNorm2d(c_out),
-            nn.ReLU6(inplace=INPLACE)
-        )
+    def __init_weights__(self):
 
-    def Pool(self, typ):
-        if typ == 'max':
-            return nn.MaxPool2d(2, stride=2)
+        nn.init.kaiming_normal_(self.saliency.weight)
+        nn.init.constant_(self.saliency.bias, 0.0)
 
+    def forward(self, x):
+        x1 = self.encode_image(x)
+        x1 = self.decoder1(x1)
+        x1 = self.decoder2(x1)
+        x1 = self.decoder3(x1)
+        sal = self.saliency(x1)
+        sal = F.relu(sal, inplace=True)
+        return sal
 
 if __name__ == "__main__":
     sample_input = torch.zeros(1, 3, 256, 320).cuda()
     model = Model().cuda()
-    a = model(sample_input)
-    print(a.shape)
-    model.train()
-    n_param = sum(p.numel() for p in model.parameters())
-    print(model(sample_input).shape)
-    print(n_param)
+    model(sample_input)
+    # flops, params = profile(model.cuda(), inputs=(sample_input,))
+    # g_flops = flops / float(1024 * 1024 * 1024)
+    # m_params = params / float(1024 * 1024)
+    # line_1 = 'proned[%s]\tGFLOPs=%sG\tparamsize=%sM\n' % ('resnetsal', round(g_flops, 4), round(m_params, 4))
+    # print(line_1)
+    pass
